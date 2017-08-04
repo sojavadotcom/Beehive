cd263ac3b9090894ea97d0c437fefa1b5d22881a
yuhuastones <yuhuastones@gmail.com>
2017-08-04 16:56:46 +0800

    新医改测算病历导入程序；东华HIS后台登录程序

--- Beehive/JavaSource/com/sojava/beehive/common/datasource/DBConnector.java
+++ Beehive/JavaSource/com/sojava/beehive/common/datasource/DBConnector.java
@@ -1,10 +1,26 @@
 package com.sojava.beehive.common.datasource;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class DBConnector {
-	public static Connection getConnect() throws Exception {
+	private String DB_DRIVER_NAME;
+	private String DB_URL;
+	private String DB_USER_NAME;
+	private String DB_PASSWORD;
+
+	public DBConnector(String driverName, String url, String userName, String password) {
+		this.DB_DRIVER_NAME = driverName;
+		this.DB_URL = url;
+		this.DB_USER_NAME = userName;
+		this.DB_PASSWORD = password;
+	}
+	public Connection getConnect() throws Exception {
 		Connection connect = null;
+		Class.forName(DB_DRIVER_NAME);
+		connect = DriverManager.getConnection(DB_URL, DB_USER_NAME, DB_PASSWORD);
+		connect.setAutoCommit(false);
+
 		return connect;
 	}
 }
