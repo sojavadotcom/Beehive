bb56766c95df83890cc77c4444425c5c697e8026
yuhuastones <yuhuastones@gmail.com>
2018-05-07 16:01:08 +0800

    struts2升级至2.5.16，逐步调整系统内部组件

--- Beehive/JavaSource/com/sojava/beehive/framework/dispatcher/ExecuteFilter.java
+++ Beehive/JavaSource/com/sojava/beehive/framework/dispatcher/ExecuteFilter.java
@@ -16,7 +16,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.apache.struts2.dispatcher.ng.filter.StrutsPrepareAndExecuteFilter;
+import org.apache.struts2.dispatcher.filter.StrutsPrepareAndExecuteFilter;
 
 import com.sojava.beehive.framework.component.user.bean.UserInfo;
 import com.sojava.beehive.framework.exception.ErrorException;
