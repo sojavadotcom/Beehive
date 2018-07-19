bc3ebe63dff559c1cab866b99739879162f561e4
yuhuastones <yuhuastones@gmail.com>
2018-07-19 15:13:56 +0800

    修改启动时间和运行方式，可用状态

--- Beehive/JavaSource/com/sojava/beehive/framework/component/catcher/action/Nhfpc.java
+++ Beehive/JavaSource/com/sojava/beehive/framework/component/catcher/action/Nhfpc.java
@@ -7,6 +7,7 @@ import com.sojava.beehive.framework.util.FormatUtil;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -17,6 +18,7 @@ import java.util.regex.Pattern;
 
 import javax.annotation.Resource;
 
+import org.apache.http.Header;
 import org.apache.http.HttpHost;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -37,10 +39,16 @@ public class Nhfpc {
 	private final String HOST = "www.nhfpc.gov.cn";
 	private final String URL = "http://www.nhfpc.gov.cn/interview/MyJsp.jsp";
 	private final String USER_AGENT = "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; WOW64; Trident/7.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E)";
-	private final String ACCEPT = "application/x-ms-application, image/jpeg, application/xaml+xml, image/gif, image/pjpeg, application/x-ms-xbap, */*";
+	private final String ACCEPT = "text/html, application/x-ms-application, image/jpeg, application/xaml+xml, image/gif, image/pjpeg, application/x-ms-xbap, */*";
 	private final String ACCEPT_LANGUAGE = "zh-CN";
 	private final String ACCEPT_ENCODING = "gzip, deflate";
 	private final String CONNECTION = "Keep-Alive";
+
+	private String JSESSIONID = "4B1356F7D9618141000B33CF628D316A";
+	private String SECURITY_SESSION_VERIFY = null;
+	private String FSSBBIl1UgzbN7N80T = null;
+	private String FSSBBIl1UgzbN7N80S = null;
+	private String BANGGOO_NUVA_COOKIE = null;
 
 	private HttpClient client = HttpClientBuilder.create().build();
 
@@ -48,49 +56,59 @@ public class Nhfpc {
 	private Date currDate = null;
 	private final Date ed = new Date();
 
-	private final long INTERVAL=1000*5;
-
 	@Resource private CatchArticleDao catchArticleDao;
 
-	@Scheduled(cron = "0 */1 * * * ?")
+	public static void main(String[] args) {
+		Nhfpc n = new Nhfpc();
+		Calendar c = Calendar.getInstance();
+		c.set(Calendar.YEAR, 2015);
+		c.set(Calendar.MONTH, 5);
+		c.set(Calendar.DAY_OF_MONTH, 22);
+		n.currDate = c.getTime();
+		try {
+			n.getPageContent(n.URL);
+		}
+		catch(Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	@Scheduled(cron = "6 2 0/10,11,14,16,18,20,22 * * ?")
 	public void execute() throws Exception {
 
 		try {
-			currDate = catchArticleDao.getLastDate();
-			currDate = currDate == null ? DEFAULT_DATE : currDate;
-			NhfpcProcessor np = new NhfpcProcessor();
-			np.start();
-/*
-			while(currDate.getTime() <= ed.getTime()) {
-				String content = getPageContent(URL);
-				List<Object> list = parse(content);
-				int _totalPage = 0, _currPage = 0;
+			if (currDate == null) {
+				currDate = catchArticleDao.getLastDate();
+				currDate = currDate == null ? DEFAULT_DATE : currDate;
+			}
+			String content = getPageContent(URL);
+			List<Object> list = parse(content);
+			int _totalPage = 0, _currPage = 0;
 
-				for(Object item: list) {
-					if (item instanceof Properties) {
-						_totalPage = Integer.parseInt(((Properties) item).getProperty("totalPage"));
-						_currPage = Integer.parseInt(((Properties) item).getProperty("currPage"));
-					} else {
-						String _title = ((Map<String, String>) item).get("title");
-						String _url = ((Map<String, String>) item).get("url");
-						String _date = ((Map<String, String>) item).get("date");
-
-						catchArticleDao.save("卫健委每日更新", _title, _url, _date);
-					}
-				}
-				if (_currPage < _totalPage) {
-					currPage = ++ _currPage;
+			for(Object item: list) {
+				if (item instanceof Properties) {
+					_totalPage = Integer.parseInt(((Properties) item).getProperty("totalPage"));
+					_currPage = Integer.parseInt(((Properties) item).getProperty("currPage"));
 				} else {
-					currPage = 1;
-					long _time = currDate.getTime() + 86400000L;
-					if (_time <= ed.getTime()) {
-						currDate.setTime(_time);
-					} else {
-						currDate.setTime(ed.getTime());
-					}
+					String _title = ((Map<String, String>) item).get("title");
+					String _url = ((Map<String, String>) item).get("url");
+					String _date = ((Map<String, String>) item).get("date");
+
+					catchArticleDao.save("卫健委每日更新", _title, _url, _date);
 				}
 			}
-*/
+			if (_currPage < _totalPage) {
+				currPage = ++ _currPage;
+			} else {
+				currPage = 1;
+				long _time = currDate.getTime() + 86400000L;
+				if (_time <= ed.getTime()) {
+					currDate.setTime(_time);
+				} else {
+					currDate.setTime(ed.getTime());
+				}
+			}
 		}
 		catch(ErrorException ex) {}
 	}
@@ -105,8 +123,34 @@ public class Nhfpc {
 		request.setHeader("User-Agent", USER_AGENT);
 		request.setHeader("Accept-Encoding", ACCEPT_ENCODING);
 		request.setHeader("Connection", CONNECTION);
+		String cookie = "";
+		if (JSESSIONID != null) cookie = JSESSIONID;
+		if (SECURITY_SESSION_VERIFY != null) cookie = (cookie.length() > 0 ? ";" : "") + SECURITY_SESSION_VERIFY;
+		if (BANGGOO_NUVA_COOKIE != null) cookie = (cookie.length() > 0 ? ";" : "") + BANGGOO_NUVA_COOKIE;
+		if (FSSBBIl1UgzbN7N80T != null) {
+			cookie = (cookie.length() > 0 ? ";" : "") + FSSBBIl1UgzbN7N80T + ";" + FSSBBIl1UgzbN7N80S;
+		}
+		request.setHeader("Cookie", cookie);
+
 		HttpHost host = new HttpHost(HOST);
 		HttpResponse response = client.execute(host, request);
+		for (Header header : response.getAllHeaders()) {
+			String name = header.getName();
+			String value = header.getValue();
+			if (name.equalsIgnoreCase("Set-Cookie")) {
+				if (value.indexOf("JSESSIONID") == 0) {
+					JSESSIONID = value.substring(0, value.indexOf(";"));
+				} else if (value.indexOf("SECURITY_SESSION_VERIFY") == 0) {
+					SECURITY_SESSION_VERIFY = value.substring(0, value.indexOf(";"));
+				} else if (value.indexOf("FSSBBIl1UgzbN7N80T") == 0) {
+					FSSBBIl1UgzbN7N80T = value.substring(0, value.indexOf(";"));
+				} else if (value.indexOf("FSSBBIl1UgzbN7N80S") == 0) {
+					FSSBBIl1UgzbN7N80S = value.substring(0, value.indexOf(";"));
+				} else if (value.indexOf("banggoo.nuva.cookie") == 0) {
+					BANGGOO_NUVA_COOKIE = value.substring(0, value.indexOf(";"));
+				}
+			}
+		}
 		BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
 
 		StringBuffer result = new StringBuffer();
@@ -200,72 +244,4 @@ public class Nhfpc {
 		this.catchArticleDao = catchArticleDao;
 	}
 
-	class NhfpcProcessor extends Thread {
-
-		@SuppressWarnings("unchecked")
-		@Override
-		public void run() {
-			super.run();
-
-			try {
-				while(currDate.getTime() <= ed.getTime()) {
-					String content = getPageContent(URL);
-					List<Object> list = parse(content);
-					int _totalPage = 0, _currPage = 0;
-
-					for(Object item: list) {
-						if (item instanceof Properties) {
-							_totalPage = Integer.parseInt(((Properties) item).getProperty("totalPage"));
-							_currPage = Integer.parseInt(((Properties) item).getProperty("currPage"));
-						} else {
-							String _title = ((Map<String, String>) item).get("title");
-							String _url = ((Map<String, String>) item).get("url");
-							String _date = ((Map<String, String>) item).get("date");
-
-							catchArticleDao.save("卫健委每日更新", _title, _url, _date);
-						}
-					}
-					if (_currPage < _totalPage) {
-						currPage = ++ _currPage;
-					} else {
-						currPage = 1;
-						long _time = currDate.getTime() + 86400000L;
-						if (_time <= ed.getTime()) {
-							currDate.setTime(_time);
-						} else {
-							currDate.setTime(ed.getTime());
-						}
-					}
-				}
-			}
-			catch(ErrorException ex) {}
-			catch(Exception ex) {
-				new ErrorException(this.getClass(), ex);
-			}
-		}
-
-		public String getPageContent(String url) throws Exception {
-
-			RequestConfig defaultConfig = RequestConfig.custom().setCookieSpec(CookieSpecs.STANDARD).build();
-			HttpGet request = new HttpGet(url + "?curr_page=" + currPage + "&date=" + FormatUtil.DATE_FORMAT.format(currDate));
-			request.setConfig(defaultConfig);
-			request.setHeader("Accept", ACCEPT);
-			request.setHeader("Accept-Language", ACCEPT_LANGUAGE);
-			request.setHeader("User-Agent", USER_AGENT);
-			request.setHeader("Accept-Encoding", ACCEPT_ENCODING);
-			request.setHeader("Connection", CONNECTION);request.setHeader("Cookie", "JSESSIONID=5DCA4C846C9070A2F37B2AC4537455FB; banggoo.nuva.cookie=3|W073h|W073d; FSSBBIl1UgzbN7N80T=2dJFGibxMNBHDtS8srpeaSZK6ge_j2JLrosCCWzs9AA7pg30WxpMKR8qaeE_30Y92kg3HZxPO.oGawmbAPdErOUP4ijgzOJ2Ye_Ji3sK2gLkJX54dD4AKJnTd4OJ_PTHgNXDYuXT8BuBE_PQNIxUrXTIfS6qBTV3TK5BFPHHyqVgaZSwsUbZOsi9aaKXZf5T.8R2.FMx2P8dH8ztRdUnOzLZ8QimHgn7MlrE76fowLxhoLwb33Wgi6Dkj6fMbaf4rzTG1cwXm1AZYM.pKPm984Ru5sdE6l20.0o3wW1IQrcVQwa; FSSBBIl1UgzbN7N80S=HtSzlGPn9fgyMWKQMySu3nbXYKrKzNPWFl5OrHNFEBZDkBRO93yscLlG5u1GZbbs; security_session_verify=389d686db75a6c6650c9988b1ec84794");
-			HttpHost host = new HttpHost(HOST);
-			sleep(INTERVAL);
-			HttpResponse response = client.execute(host, request);
-			BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
-
-			StringBuffer result = new StringBuffer();
-			String line = "";
-			while ((line = rd.readLine()) != null) {
-				result.append(line + "\n");
-			}
-
-			return result.toString();
-		}
-	}
 }
