d8e11bce3a7df2fe251783bd8898bb16b531574d
yuhuastones <yuhuastones@gmail.com>
2019-09-23 17:30:03 +0800

    卫健委最新消息信息抓取导入处理

---
+++ Beehive/JavaSource/com/sojava/beehive/framework/component/datatrans/action/ImportExpense.java
@@ -0,0 +1,31 @@
+package com.sojava.beehive.framework.component.datatrans.action;
+
+import com.sojava.beehive.framework.component.catcher.action.NhfpcSchedule;
+
+import java.util.Date;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+public class ImportExpense {
+
+	private static boolean autoClose = false;
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext ctx = new FileSystemXmlApplicationContext("classpath:conf/spring/applicationContext.xml");
+		try {
+			NhfpcSchedule schedule = (NhfpcSchedule) ctx.getBean("nhfpcSchedule");
+			Date dt = new Date();
+			dt.setTime(1533830400000l);
+			schedule.setCurrDate(dt);
+			System.out.println(schedule.getCurrDate());
+		}
+		catch(Exception ex) {
+			ex.printStackTrace();
+		}
+		finally {
+			if (autoClose) ctx.close();
+		}
+	}
+
+}
