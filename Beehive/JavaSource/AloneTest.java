f64dee1e767fa203e8ba27cf244e93a6637fb1c2
yuhuastones@gmail.com <yuhuastones@gmail.com>
2019-09-30 07:26:12 +0800

    测试程序日常更新

--- Beehive/JavaSource/AloneTest.java
+++ Beehive/JavaSource/AloneTest.java
@@ -1,22 +1,19 @@
-import com.sojava.beehive.framework.component.catcher.action.NhfpcSchedule;
+import com.sojava.beehive.framework.component.inpatienthomepage.service.HomepageCheckerService;
 
-import java.util.Date;
+import java.io.File;
 
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class AloneTest {
 
-	private static boolean autoClose = false;
+	private static boolean autoClose = true;
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext ctx = new FileSystemXmlApplicationContext("classpath:conf/spring/applicationContext.xml");
 		try {
-			NhfpcSchedule schedule = (NhfpcSchedule) ctx.getBean("nhfpcSchedule");
-			Date dt = new Date();
-			dt.setTime(1533830400000l);
-			schedule.setCurrDate(dt);
-			System.out.println(schedule.getCurrDate());
+			HomepageCheckerService checker = (HomepageCheckerService) ctx.getBean("homepageCheckerServiceImpl");
+			checker.loadCSV(new File[] {new File("/Users/John/Documents/Works/公立医院绩效考核/病案首页/TCMMS_201901-08_14023.csv")});
 		}
 		catch(Exception ex) {
 			ex.printStackTrace();
