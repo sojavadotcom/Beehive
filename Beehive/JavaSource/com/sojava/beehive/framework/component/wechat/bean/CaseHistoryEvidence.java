0365c4ceef407bf7923bd5e1acad14f5c036e56a
yuhuastones@gmail.com <yuhuastones@gmail.com>
2020-01-13 02:17:36 +0800

    增加微信取证数据表映射

---
+++ Beehive/JavaSource/com/sojava/beehive/framework/component/wechat/bean/CaseHistoryEvidence.java
@@ -0,0 +1,121 @@
+package com.sojava.beehive.framework.component.wechat.bean;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.Date;
+
+
+/**
+ * The persistent class for the case_history_evidence database table.
+ * 
+ */
+@Entity
+@Table(name="case_history_evidence")
+@NamedQuery(name="CaseHistoryEvidence.findAll", query="SELECT c FROM CaseHistoryEvidence c")
+public class CaseHistoryEvidence implements Serializable {
+	private static final long serialVersionUID = -1795354356510863248L;
+
+	@Id
+	@SequenceGenerator(name="CASE_HISTORY_EVIDENCE_ID_GENERATOR", sequenceName="QUALITY.CASE_HISTORY_EVIDENCE_ID")
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="CASE_HISTORY_EVIDENCE_ID_GENERATOR")
+	private Integer id;
+
+	@Column(name="checker_name")
+	private String checkerName;
+
+	private String code;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name="create_time")
+	private Date createTime;
+
+	private String data;
+
+	@Column(name="item_label")
+	private String itemLabel;
+
+	@Column(name="item_num")
+	private Integer itemNum;
+
+	@Column(name="wx_id")
+	private String wxId;
+
+	@Column(name="wx_name")
+	private String wxName;
+
+	public CaseHistoryEvidence() {
+	}
+
+	public Integer getId() {
+		return this.id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getCheckerName() {
+		return this.checkerName;
+	}
+
+	public void setCheckerName(String checkerName) {
+		this.checkerName = checkerName;
+	}
+
+	public String getCode() {
+		return this.code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public Date getCreateTime() {
+		return this.createTime;
+	}
+
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+
+	public String getData() {
+		return this.data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public String getItemLabel() {
+		return this.itemLabel;
+	}
+
+	public void setItemLabel(String itemLabel) {
+		this.itemLabel = itemLabel;
+	}
+
+	public Integer getItemNum() {
+		return this.itemNum;
+	}
+
+	public void setItemNum(Integer itemNum) {
+		this.itemNum = itemNum;
+	}
+
+	public String getWxId() {
+		return this.wxId;
+	}
+
+	public void setWxId(String wxId) {
+		this.wxId = wxId;
+	}
+
+	public String getWxName() {
+		return this.wxName;
+	}
+
+	public void setWxName(String wxName) {
+		this.wxName = wxName;
+	}
+
+}
